7293db0ffc87d58da8bf2a60957a69efe9e360c9
Tomás R. <tomaslopezraigal@gmail.com>
2024-11-28 19:11:57 +0100

    Layout

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,13 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layout')
+
+@section('content')
     <h1>Contact 1</h1>
     <p>{{ $name }}</p>
 
@@ -20,5 +13,4 @@
     @foreach([1, 2, 3, 4, 5] as $number)
         <p>{{ $number }}</p>
     @endforeach
-</body>
-</html>
+@endsection
