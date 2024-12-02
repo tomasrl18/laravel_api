630b9067f8d34aded5601ce130141f03c960fc74
Tomás R. <tomaslopezraigal@gmail.com>
2024-12-02 23:18:30 +0100

    Create: Views, CSRF

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
     <h2>Create a Post</h2>
-    <form action="" method="post" class="form">
+    <form action="{{ route('post.store') }}" method="post" class="form">
+        @csrf
+
         <label for="title">Title</label>
         <input type="text" name="title" id="title" class="input">
 
