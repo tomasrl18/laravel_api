312b3210e6df68eea0f7ac37af440d5538be61b5
Tomás R. <tomaslopezraigal@gmail.com>
2024-12-09 19:12:42 +0100

    Old value

--- resources/views/dashboard/posts/fragment/form.blade.php
+++ resources/views/dashboard/posts/fragment/form.blade.php
@@ -1,26 +1,28 @@
 @csrf
 
 <label for="title">Title</label>
-<input type="text" name="title" id="title" class="input" value="{{ $post->title }}">
+<input type="text" name="title" id="title" class="input" value="{{ old("title", $post->title) }}">
 
 <label for="slug">Slug</label>
-<input type="text" name="slug" id="slug" class="input" value="{{ $post->slug }}">
+<input type="text" name="slug" id="slug" class="input" value="{{ old("slug", $post->slug) }}">
 
 <label for="description">Description</label>
-<textarea name="description" id="description" class="textarea">{{ $post->description ?? '' }}</textarea>
+<textarea name="description" id="description" class="textarea">{{ old("description", $post->description) }}</textarea>
 
 <label for="content">Content</label>
-<textarea name="content" id="content" class="textarea">{{ $post->content ?? '' }}</textarea>
+<textarea name="content" id="content" class="textarea">{{ old("content", $post->content) }}</textarea>
 
 <label for="category_id">Pick a category</label>
 <select name="category_id" id="category_id" class="select">
     @foreach($categories as $title => $id)
-        <option value="{{ $id }}" {{ $post->category_id == $id ? 'selected' : '' }}>{{ $title }}</option>
+        <option value="{{ $id }}" {{ old("category_id", $post->category_id) == $id ? 'selected' : '' }}>
+            {{ $title }}
+        </option>
     @endforeach
 </select>
 
 <label for="posted">Published</label>
 <select name="posted" id="posted" class="select">
-    <option value="no" {{ $post->posted == 'no' ? 'selected' : '' }}>No</option>
-    <option value="yes" {{ $post->posted == 'yes' ? 'selected' : '' }}>Yes</option>
+    <option value="no" {{ old("posted", $post->posted) == 'no' ? 'selected' : '' }}>No</option>
+    <option value="yes" {{ old("posted", $post->posted) == 'yes' ? 'selected' : '' }}>Yes</option>
 </select>
