162c7470dde1b5e98ffe37bddc9e766170141b52
Tomás R. <tomaslopezraigal@gmail.com>
2024-12-10 00:21:32 +0100

    Grouped routes

--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,7 @@ Route::get('/', function () {
     return view('index');
 })->name('index');
 
-Route::resource('post', PostController::class);
-Route::resource('category', CategoryController::class);
+Route::group(['prefix' => '/dashboard'], function () {
+    Route::resource('post', PostController::class);
+    Route::resource('category', CategoryController::class);
+});
