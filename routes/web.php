ffabfe8a64328424fa18cca8583d1bef18f37b1a
Tomás R. <tomaslopezraigal@gmail.com>
2024-12-10 12:20:35 +0100

    Install and config laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\Dashboard\{PostController, CategoryController};
-
 Route::get('/', function () {
-    return view('index');
-})->name('index');
-
-Route::group(['prefix' => '/dashboard'], function () {
-    Route::resource('post', PostController::class);
-    Route::resource('category', CategoryController::class);
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
