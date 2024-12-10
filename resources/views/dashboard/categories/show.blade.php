4886fe16b7dfe07a84cbeefe4d07a65d108b67f0
Tomás R. <tomaslopezraigal@gmail.com>
2024-12-10 23:41:19 +0100

    Translated some text to english

--- resources/views/dashboard/categories/show.blade.php
+++ resources/views/dashboard/categories/show.blade.php
@@ -9,10 +9,10 @@
 
         <div class="flex justify-center">
             <div>
-                <h2 class="text-xl font-semibold text-gray-700 mb-3">Detalles de la Categoría</h2>
+                <h2 class="text-xl font-semibold text-gray-700 mb-3">Category Details</h2>
                 <div class="flex items-center justify-center">
                     <svg class="w-5 h-5 text-gray-500 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-                    <span>Creada: {{ $category->created_at->format('d/m/Y') }}</span>
+                    <span>Created: {{ $category->created_at->format('d/m/Y') }}</span>
                 </div>
             </div>
         </div>
