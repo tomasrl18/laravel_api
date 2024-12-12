906c1ec9cde37baf1c6e36a68da2f1acfc8d0491
Tomás R. <tomaslopezraigal@gmail.com>
2024-12-12 20:41:40 +0100

    Styling the list

--- resources/views/components/blog/post/index.blade.php
+++ resources/views/components/blog/post/index.blade.php
@@ -1,12 +1,12 @@
-<div>
-    <div class="pt-5">
+<div class="container mx-auto px-4">
+    <div class="pt-5 space-y-4">
         @foreach($posts as $post)
-            <div class="card card-gray-100 mt-2">
-                <h3>{{ $post->title }}</h3>
+            <div class="bg-gray-100 border border-gray-300 rounded-lg p-4 shadow hover:shadow-lg transition duration-200">
+                <h3 class="text-xl font-semibold text-gray-800 mb-2">{{ $post->title }}</h3>
 
-                <a href="#">Ruta de show</a>
+                <a href="#" class="text-blue-600 hover:underline font-medium">Ruta de show</a>
 
-                <p>{{ $post->description }}</p>
+                <p class="text-gray-600 mt-3">{{ $post->description }}</p>
             </div>
         @endforeach
     </div>
