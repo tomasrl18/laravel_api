9339d603b356455bb2296c93447ab053ca952326
Tomás R. <tomaslopezraigal@gmail.com>
2024-12-13 12:50:32 +0100

    Example of arrow function

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,5 +1,5 @@
 @extends('blog.layout')
 
 @section('content')
-    <x-blog.post.show :post="$post"></x-blog.post.show>
+    <x-blog.post.show :post="$post" data-id="1" class="demo"></x-blog.post.show>
 @endsection
--- resources/views/components/blog/post/show.blade.php
+++ resources/views/components/blog/post/show.blade.php
@@ -10,6 +10,8 @@
             </div>
         @endif
 
+        {{ $attributes->filter((fn(string $value, string $key) => $key == 'data-id')) }}
+
         <h1 class="text-3xl font-bold text-gray-800 mb-4">{{ $post->title }}</h1>
 
         <div class="mt-4 text-gray-600 text-center">
