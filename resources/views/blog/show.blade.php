a172acb101676c25155ba5426ab0d96f3b078191
Tomás R. <tomaslopezraigal@gmail.com>
2024-12-13 12:53:55 +0100

    Dynamic components

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,5 +1,6 @@
 @extends('blog.layout')
 
 @section('content')
-    <x-blog.post.show :post="$post" data-id="1" class="demo"></x-blog.post.show>
+    <x-blog.post.show :post="$post"></x-blog.post.show>
+    <x-dynamic-component component="blog.post.show" :post="$post" />
 @endsection
