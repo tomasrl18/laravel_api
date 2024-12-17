34437be4b13d84339ac35f72a389f8e2a51aa755
Tomás R. <tomaslopezraigal@gmail.com>
2024-12-17 12:29:35 +0100

    Create controllers of categories and posts and test json response

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\API\{CategoryController, PostController};
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::resource('category', CategoryController::class)->except(['create', 'edit']);
+Route::resource('post', PostController::class)->except(['create', 'edit']);
