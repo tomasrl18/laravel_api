e67de4736a27c90b416607a121ea0b370f05aeb6
Tomás R. <tomaslopezraigal@gmail.com>
2024-12-18 21:28:27 +0100

    Install Material Design Icons

--- resources/js/vue/main.js
+++ resources/js/vue/main.js
@@ -5,6 +5,8 @@ import axios from 'axios';
 import Oruga from '@oruga-ui/oruga-next';
 import '@oruga-ui/theme-oruga/dist/oruga.css';
 
+import '@mdi/font/css/materialdesignicons.min.css';
+
 import App from './App.vue';
 
 const app = createApp(App);
