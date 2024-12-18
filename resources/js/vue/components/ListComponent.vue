b11c31c6100ad97af313218d363cc655a62d8f6d
Tomás R. <tomaslopezraigal@gmail.com>
2024-12-18 19:57:08 +0100

    First steps with Oruga UI

--- resources/js/vue/components/ListComponent.vue
+++ resources/js/vue/components/ListComponent.vue
@@ -1,5 +1,13 @@
 <template>
     <div>
         <h1>Post List</h1>
+
+        <o-button @click="() => { console.log('Hello world') }">
+            Click me
+        </o-button>
+
+        <o-field label="Email" variante="danger">
+            <o-input type="email" value="tomas@test.com"></o-input>
+        </o-field>
     </div>
 </template>
--- resources/js/vue/main.js
+++ resources/js/vue/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue';
 
+import Oruga from '@oruga-ui/oruga-next';
+import '@oruga-ui/theme-oruga/dist/oruga.css';
+
 import App from './App.vue';
+
 const app = createApp(App);
+app.use(Oruga);
 app.mount('#app');
