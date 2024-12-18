ae92fef26d61897075bbce542c8c348e5bf2e75c
Tomás R. <tomaslopezraigal@gmail.com>
2024-12-18 18:33:51 +0100

    Handle exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,11 @@
 <?php
 
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Configuration\{
+    Exceptions,
+    Middleware
+};
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (NotFoundHttpException $exception, $request) {
+            if($request->expectsJson()) {
+                return response()->json(['message' => 'Not Found'], 404);
+            }
+        });
     })->create();
